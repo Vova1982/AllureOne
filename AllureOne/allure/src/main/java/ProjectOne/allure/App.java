ffdf46af8ec1d45069fea355a855ac637911475f
Vova1982 <volodymyr.dziuba@gmail.com>
2020-12-21 16:22:40 -0500

    First commit

---
+++ AllureOne/allure/src/main/java/ProjectOne/allure/App.java
@@ -0,0 +1,13 @@
+package ProjectOne.allure;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ AllureOne/allureId/src/main/java/allure/allureId/App.java
@@ -0,0 +1,13 @@
+package allure.allureId;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
